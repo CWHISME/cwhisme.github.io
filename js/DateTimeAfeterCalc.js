b2c4c3cd41015f8505991cce002637297c802f2b
cwhisme <cwhisme@qq.com>
2021-06-08 18:24:09 +0800

    Site updated: 2021-06-08 18:24:04

--- js/DateTimeAfeterCalc.js
+++ js/DateTimeAfeterCalc.js
@@ -1,1 +1,40 @@
-var nowStr="今天",dayStr="天前",monthStr="个月前",monthLastStr="上个月",yearStr="年前",yearLastStr="去年";$(document).ready(function(){$("span#dateTimeAfter").each(function(){var t=$(this).text().trim().split("-"),e=new Date(t[0],t[1]-1,t[2]),r=new Date,a=r.getFullYear()-e.getFullYear();a=0==a?0==(t=r.getMonth()-e.getMonth())?0==(e=r.getDate()-e.getDate())?nowStr:e+dayStr:1==t?monthLastStr:t+monthStr:1==a?yearLastStr:a+yearStr,$(this).text(a)})});
+//用于计算当前时间距离指定日期，过了多久
+//替换id 为 dateTimeAfter 的目标
+//需求：目标的text内容为日期，格式为：<span>%Y/%m/%d</span>
+//author: CWHISME
+//git: https://github.com/CWHISME
+//2020.1.14
+
+var nowStr = "今天";
+var dayStr = "天前";
+var monthStr = "个月前";
+var monthLastStr = "上个月";
+var yearStr = "年前";
+var yearLastStr = "去年";
+
+$(document).ready(function () {
+    $('span#dateTimeAfter').each(function () {
+        var dateStr = $(this).text().trim();
+        var dateArray = dateStr.split("-");
+        //月份的数值，介于 0 ~ 11 之间。用本地时间表示。
+        var date = new Date(dateArray[0], dateArray[1] - 1, dateArray[2]);
+        var dateNow = new Date();
+        var finalStr;
+        var year = dateNow.getFullYear() - date.getFullYear();
+        if (year == 0) {
+            var month = dateNow.getMonth() - date.getMonth();
+            if (month == 0) {
+                var day = dateNow.getDate() - date.getDate();
+                if (day == 0)
+                    finalStr = nowStr;
+                else finalStr = day + dayStr;
+            }else if (month == 1) {
+                finalStr = monthLastStr;
+            }
+            else finalStr = month + monthStr;
+        } else if (year == 1) {
+            finalStr = yearLastStr;
+        } else finalStr = year + yearStr;
+        $(this).text(finalStr);
+    });
+});
